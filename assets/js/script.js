a3167d23ad88570b6963b1342a8147f4e2c87c6b
Edita P <blugirlnyc@hotmail.com>
2021-02-27 00:25:20 -0500

    made some changes

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,101 +1,107 @@
-var timerEl = document.querySelector(".timer");
+var timerEl = document.getElementById('time');
 var secondsLeft = 75;
 var timerInterval;
 
 var startButton = document.getElementById('start-btn')
-var questionContainerEl = document.getElementById('question-container')
-var questionEl = document.getElementById('question')
+var questionEl = document.getElementById('question-container')
+
 var answerButtonsEl = document.getElementById('answer-buttons')
+var rightOrWrong = document.getElementById('right-wrong')
 
 var currentQuestionIndex = 0;
-var questionsCorrect = 0;
 
-var initialsEl = document.querySelector("#user-initials");
-var submitBtn = document.querySelector("#submit");
-var highScore;
+var initialsEl = document.getElementById("user-initials");
+var submitBtn = document.getElementById("submit");
 
-
-startButton.addEventListener('click', startGame)
-
-//if all questions answered or timer = 0, game is over
-function setTimer() {
-    currentQuestionIndex = 0;
-    secondsLeft = 75;
-    questionsCorrect = 0;
-    timerInterval = setInterval(function () {
-        secondsLeft--;
-        timerEl.textContent = ("Time: ") + secondsLeft;
-
-        if (secondsLeft === 0) {
-            clearInterval(timerInterval);
-            addScoreTotal();
-        }
-    }, 1000);
-}
+startButton.onclick = startGame;
 
 //Start quiz, hide pg
 function startGame() {
     var startPageEl = document.getElementById('start-page');
     startPageEl.setAttribute('class', 'hide');
-    //currentQuestionIndex = 0;
-    questionContainerEl.classList.remove('hide');
+    questionEl.removeAttribute('class');
     timerInterval = setInterval(setTimer, 1000);
-    showQuestion()
+    timerEl.textContent = secondsLeft;
+    showQuestion();
 }
 
 //question appears - multiple choice, 4 answers
-function showQuestion(question) {
-    questionEl.innerHTML = "";
-    //answerButtonsEl.innerHTML = "";
+function showQuestion() {
+    var currentQuestion = questions[currentQuestionIndex];
+    var questionHeader = document.getElementById('question-header');
+    questionHeader.textContent = currentQuestion.question;
 
-    var currentQuestion = questions[currentQuestionIndex].question;
-    var currentAnswers = questions[currentQuestionIndex].choices;
+    answerButtonsEl.innerHTML = "";
+    
+    currentQuestion.choices.forEach(function(choice, i) {
+        var answerChoice = document.createElement('button');
+        answerChoice.setAttribute('class', 'choice');
+        answerChoice.setAttribute('value', choice);
 
-    //var questionHeaderEl = document.getElementById('question');
-    questionEl.textContent = currentQuestion;
-    questionContainerEl.appendChild(questionEl);
+        answerChoice.textContent = i + 1 + '. ' + choice;
 
-    currentAnswers.forEach(choice => {
-        var button = document.createElement('button');
-        //button.innerText = choice.text;
-        button.textContent = choice;
-        button.classList.add('btn');
+        answerChoice.onclick = selectAnswer;
+        answerButtonsEl.appendChild(answerChoice);
 
-        button.addEventListener('click', selectAnswer)
-        answerButtonsEl.appendChild(button);
-
-    })
-    questionContainerEl.appendChild(answerButtonsEl);
+    });
 }
 
-function selectAnswer(event) {
-    var selectedButton = event.target;
-    var correct = selectedButton.dataset.correct;
-    setStatusClass() //need to finish this function
-    if (this.value !== questions[currentQuestionIndex].answer) {
-        time -= 10;
-    }
-}
 
 //after question is answered, another question appears 4x, "Correct!" or "Wrong!" appears
 //if question is answered incorrectly, 10 secs are subtracted from clock
+function selectAnswer() {
+    
+    if (this.value !== questions[currentQuestionIndex].answer) {
+        secondsLeft -= 15;
 
+        if (secondsLeft < 0) { 
+            secondsLeft = 0;
+    }
 
-function setStatusClass(element, correct) {
-    clearStatusClass(element)
-    if (correct) {
-        element.classList.add('correct')
+    timerEl.textContent = secondsLeft;
+
+    rightOrWrong.textContent = "Wrong"
+
     } else {
-        element.classList.add('wrong')
+        rightOrWrong.textContent = "Correct";
+    }
+
+    rightOrWrong.setAttribute("class", "right-wrong");
+    setTimeout(function() {
+        rightOrWrong.setAttribute('class', 'right-wrong hide');
+    }, 1000);
+
+    currentQuestionIndex++;
+
+    if (currentQuestionIndex === questions.length) {
+        addScoreTotal();
+    } else {
+        showQuestion();
     }
 }
 
-function clearStatusClass(element) {
-    element.classList.remove('correct')
-    element.classList.remove('wrong')
+function addScoreTotal () {
+    clearInterval(timerInterval);
+
+    var gameEnd = document.getElementById('game-end');
+    gameEnd.removeAttribute('class');
+
+    var finalScore = document.getElementById('score');
+    finalScore.textContent = secondsLeft;
+
+    questionEl.setAttribute('class', 'hide');
 }
 
-const questions = [
+function setTimer() {
+    secondsLeft--;
+    timerEl.textContent = secondsLeft;
+
+    if (secondsLeft <= 0) {
+        addScoreTotal();
+    }
+}
+
+var questions = [
     {
         question: "Commonly used data types DO NOT include:",
         choices: ["1. strings", "2. booleans", "3. alerts", "4. numbers"],
@@ -126,19 +132,21 @@ const questions = [
 
 //at end of game, user can save their initials & score - submit button
 
-renderHighScores();
+function saveHighScores() {
+    var initials = initialsEl.value.trim();
+    
+    if (initials !== "") {
+        var highscores = JSON.parse(window.localStorage.getItem('highscores')) || [];
+        var newScore = {
+            score: secondsLeft,
+            initials: initials
+        };
 
-function addInitials(event) {
-    event.preventDefault();
-    var initialsEntered = initialsEl;
+        //save to local storage
+    }
 }
 
-function renderHighScores() {
-    var initials = localStorage.getItem("user-initials");
-    var highScore = localStorage.getItem("score");
-}
-
-/*submitBtn.addEventListener("click", function(event) {
+submitBtn.addEventListener("click", function(event) {
     event.preventDefault();
 
     var initials = document.querySelector("#user-initials");
@@ -148,7 +156,7 @@ function renderHighScores() {
 
         localStorage.setItem("user-initials", initials);
         localStorage.setItem("score", highScore);
-        renderHighScores(
-}); */
+        renderHighScores()
+}); 
 
 //choose "go back" or "clear high scores" buttons
